a24843dcac9e425bd1a2fa79bcd9b3e44a65c4b0
Joakim Larsson <joakim.larsson2@helsingborg.se>
2022-09-27 16:11:29 +0200

    🎨 test tidy

--- src/visma-auth/__tests/login.test.ts
+++ src/visma-auth/__tests/login.test.ts
@@ -1,10 +1,20 @@
+import { StatusCodes } from 'http-status-codes'
 import * as request from 'supertest'
-import { createFakeServices, notImplemented, withAuthApplication } from './test-utils'
+import { createAuthApp } from '../../auth-app'
+import { Application, ApplicationContext } from '../../framework/types'
+import { AuthServices } from '../types'
+import { createFakeServices, notImplemented } from './test-utils'
+
+
+// a simple module that prevents errors to go to console.error
+const silentErrorsModule = ({ app }: ApplicationContext) => app.on('error', () => (void 0))
+
+const createTestApp = (services: AuthServices): Application => createAuthApp({ services, validateResponse: true }).use(silentErrorsModule)
 
 describe('GET /api/v1/auth/login', () => {
 	const fakeReturn = 'https://www.example.com/ninja'
 
-	it('redirects to whatever Visma says', () => withAuthApplication(
+	it('redirects (302 moved temporarily) to whatever Visma says', () => createTestApp(
 		createFakeServices({
 			visma: {
 				getSession: notImplemented,
@@ -13,12 +23,13 @@ describe('GET /api/v1/auth/login', () => {
 					sessionId: 'some-fake-session-id',
 				}),
 			},
-		}),
-		async server => {
-			const fakeReturn = 'https://www.example.com/ninja'
-			const { status, headers } = await request(server)
-				.get('/api/v1/auth/login?redirect_url=whateverInThisStageOfProcess.com')
-			expect(status).toEqual(302)
-			expect(headers['location']).toBe(fakeReturn)
 		}))
+		.run(
+			async server => {
+				const fakeReturn = 'https://www.example.com/ninja'
+				const { status, headers } = await request(server)
+					.get('/api/v1/auth/login?redirect_url=whateverInThisStageOfProcess.com')
+				expect(status).toEqual(StatusCodes.MOVED_TEMPORARILY)
+				expect(headers['location']).toBe(fakeReturn)
+			}))
 })
--- src/visma-auth/__tests/profiles.test.ts
+++ src/visma-auth/__tests/profiles.test.ts
@@ -1,21 +1,21 @@
+import { StatusCodes } from 'http-status-codes'
 import * as request from 'supertest'
 import { SigningProfile } from '../profiles'
 
-import { createFakeServices, notImplemented, withAuthApplication } from './test-utils'
+import { createFakeServices, createTestApp, notImplemented } from './test-utils'
 
 describe('GET /api/v1/auth/profiles', () => {
-	it('responds with JSON', async () => withAuthApplication(
-		createFakeServices(),
-		async server => {
-			const response = await request(server)
-				.get('/api/v1/auth/profiles')
-				.set('Accept', 'application/json')
+	it('responds with JSON', async () => createTestApp(createFakeServices())
+		.run(
+			async server => {
+				const response = await request(server)
+					.get('/api/v1/auth/profiles')
+					.set('Accept', 'application/json')
 
-			expect(response.headers['content-type']).toMatch(/json/)
-			expect(response.status).toEqual(200)        
-		}))
-	it('responds with array of SigningProfiles', async () => withAuthApplication(
-		createFakeServices(),
+				expect(response.headers['content-type']).toMatch(/json/)
+				expect(response.status).toEqual(StatusCodes.OK)        
+			}))
+	it('responds with array of SigningProfiles', async () => createTestApp(createFakeServices()).run(
 		async server => {
 			const { body } = await request(server)
 				.get('/api/v1/auth/profiles')
@@ -25,19 +25,20 @@ describe('GET /api/v1/auth/profiles', () => {
 				expect(typeof p.id).toBe('string')
 			}
 		}))
-	it('responds with 502 on malformed response', async () => withAuthApplication(
+	it('responds with 502 bad gateway on malformed response', async () => createTestApp(
 		// NOTE: This test succedds only if response validation is enabled, see createAuthApp(...)
 		createFakeServices({
 			profiles: {
 				getBestMatchingProfile: notImplemented,
 				getProfiles: () => [{ total: 'bogus' } as unknown as SigningProfile],
 			},
-		}),
-		async server => {
-			const { status } = await request(server)
-				.get('/api/v1/auth/profiles')
-				.set('Accept', 'application/json')
-			expect(status).toBe(502)
-		}
-	))
+		}))
+		.run(
+			async server => {
+				const { status } = await request(server)
+					.get('/api/v1/auth/profiles')
+					.set('Accept', 'application/json')
+				expect(status).toBe(StatusCodes.BAD_GATEWAY)
+			}
+		))
 })
